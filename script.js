c1829af72482d14fddd321a18edd10078387961c
hamzakhokar09 <163120436+hamzakhokar09@users.noreply.github.com>
2025-06-14 02:15:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function fakeLogin() {
+  alert("⚠️ ALERT: This was a phishing simulation. Never enter your real credentials on unknown sites.");
+  return false; // prevent actual form submission
+}
